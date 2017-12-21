423023d7328aaa85597e7ecd1170eadb61d6baf2
Avik Chakraborty <avik.chakraborty@lexmark.com>
2017-12-21 11:25:05 +0530

    added put and get method

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # springboot-sample-app
 
+ - Run `maven install`
  - Go to the Project directory
- - Run mvn clean insatll
  - Run `sudo docker build -t s3-boot .`
  - Get the image id of **s3-boot** using `sudo docker images --all`
  - Run `sudo docker run -p 8080:8080 <imageid>`
+ - Test `curl "http://localhost:8080"`
+ - Sample output:
+ `
+Hello - from boot service
+http://localhost:8080/putfile?bucketName=<NAME>&objectKey=<KEY>&region=<REGION>
+http://localhost:8080/getfile?bucketName=<NAME>&objectKey=<KEY>&region=<REGION>
+http://localhost:8080/deletefile?bucketName=<NAME>&objectKey=<KEY>&region=<REGION>
+ 
+ `
  
 # Upload in aws ecr
 
 
-- try to run `docker run hello-world` withoud **sudo**. if not follow this [site](https://askubuntu.com/questions/477551/how-can-i-use-docker-without-sudo) to make it happen
+- try to run `docker run hello-world` without **sudo**. if not follow this [site](https://askubuntu.com/questions/477551/how-can-i-use-docker-without-sudo) to make it happen
 - create an ECR Repository in correct region and follow below steps
 - Go to the Project directory
 - `eval $(aws ecr get-login --no-include-email --region us-east-2 --profile your-profile | sed 's|https://||')`
--- src/main/java/com/avik/HomeController.java
+++ src/main/java/com/avik/HomeController.java
@@ -1,26 +1,13 @@
-/*
- * Copyright 2016 codecentric AG
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.avik;
 
-import javax.validation.Valid;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,18 +16,99 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private static String getStringFromInputStream(InputStream is) {
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String home(ModelMap model) {
-		model.addAttribute("insertRecord", new Record());
-		return "home";
+		BufferedReader br = null;
+		StringBuilder sb = new StringBuilder();
+
+		String line;
+		try {
+
+			br = new BufferedReader(new InputStreamReader(is));
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return sb.toString();
+
 	}
+
+	
 	@ResponseBody
-	@RequestMapping(path="rest", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
+	public String testService() {
+		return "Hello - from boot service" + "\n" 
+				+ "http://localhost:8080/putfile?bucketName=<NAME>&objectKey=<KEY>&region=<REGION>" + "\n"
+				+ "http://localhost:8080/getfile?bucketName=<NAME>&objectKey=<KEY>&region=<REGION>" + "\n"
+				+ "http://localhost:8080/deletefile?bucketName=<NAME>&objectKey=<KEY>&region=<REGION>"+ "\n";
+	}
+	
+	@ResponseBody
+	@RequestMapping(path="putfile", method = RequestMethod.GET)
+	public String putObjectOverGet(@RequestParam(name = "bucketName") String bucket_name,
+			@RequestParam(name = "objectKey") String object_key, @RequestParam(name = "region") String region) {
+		System.out.format("Putting object to S3 bucket: %s of region: %s \n", object_key, bucket_name,region);
+		final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(region).build();
+		File file = new File("temp"+Math.random());
+		try {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+				return e.getMessage();
+			}
+			s3.putObject(new PutObjectRequest(bucket_name, object_key , file));
+			file.delete();
+			System.out.format("Put object %s from S3 bucket: %s of region: %s\n", object_key, bucket_name,region);
+			return "Successfully put the key";
+		} catch (AmazonServiceException e) {
+			e.printStackTrace();
+			return e.getErrorMessage();
+		}
+		finally {
+			file.delete();
+		}
+	}
+	
+	@ResponseBody
+	@RequestMapping(path="getfile", method = RequestMethod.GET)
+	public String readObjectOverGet(@RequestParam(name = "bucketName") String bucket_name,
+			@RequestParam(name = "objectKey") String object_key, @RequestParam(name = "region") String region) {
+		System.out.format("Getting object %s from S3 bucket: %s of region: %s \n", object_key, bucket_name,region);
+		final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(region).build();
+		try {
+			S3Object object = s3.getObject(new GetObjectRequest(bucket_name, object_key));
+			InputStream objectData = object.getObjectContent();			
+			return getStringFromInputStream(objectData);
+		} catch (AmazonServiceException e) {
+			e.printStackTrace();
+			return e.getErrorMessage();
+		}
+	}
+	
+	
+	@ResponseBody
+	@RequestMapping(path="deletefile", method = RequestMethod.GET)
 	public String deleteObjectOverGet(@RequestParam(name = "bucketName") String bucket_name,
 			@RequestParam(name = "objectKey") String object_key, @RequestParam(name = "region") String region) {
 		System.out.format("Deleting object %s from S3 bucket: %s of region: %s \n", object_key, bucket_name,region);
@@ -51,34 +119,7 @@ public class HomeController {
 			return "Successfully deleted the key";
 		} catch (AmazonServiceException e) {
 			e.printStackTrace();
-			return "Error";
-		}
-	}
-
-	@RequestMapping(method = RequestMethod.POST)
-	public String deleteObjectOverPost(ModelMap model, @ModelAttribute("insertRecord") @Valid Record record,
-			BindingResult result) {
-		if (!result.hasErrors()) {
-			String bucket_name = record.getBucketName();
-			String object_key = record.getObjectKey();
-			String region = record.getRegion();
-			System.out.format("Deleting object %s from S3 bucket: %s of region: %s \n", object_key, bucket_name,region);
-			final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(region).build();
-			try {
-				s3.deleteObject(bucket_name, object_key);
-				System.out.format("Deleted object %s from S3 bucket: %s of region: %s\n", object_key, bucket_name,region);
-				model.addAttribute("result", "Successfully deleted the key");
-				return home(model);
-			} catch (AmazonServiceException e) {
-				e.printStackTrace();
-				model.addAttribute("result", e.getErrorMessage());
-				return home(model);
-			}
-
-		} else {
-			System.out.format("Invalid request parameter");
-			model.addAttribute("result", "Invalid request parameter");
-			return home(model);
+			return e.getErrorMessage();
 		}
 	}
 }
